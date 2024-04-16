8a42cd6286bfa4509812a8f79fae5992a9d2641f
Leonardo Mendoza Fernadez <l.mendoza@Leonardos-MacBook-Pro.local>
2024-04-16 15:33:38 -0600

    Clone wp post

--- wordpress-cloning-script.py
+++ wordpress-cloning-script.py
@@ -29,7 +29,8 @@ def main(environment, post_id, wordpreess_staggin_username, wordpreess_staggin_p
         print(f"Failed to fetch post: {response}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    print(sys.argv)
+    if len(sys.argv) != 5:
         print("Usage: python wordpress-cloning-script.py <environment> <postID> <wordpreess_staggin_username> <wordpreess_staggin_password>")
         sys.exit(1)
     
