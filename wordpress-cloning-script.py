81fb2eb60514023821362e6be640922c17f46f71
Leonardo Mendoza Fernadez <l.mendoza@Leonardos-MacBook-Pro.local>
2024-04-16 13:35:34 -0600

    Clone wp post

--- wordpress-cloning-script.py
+++ wordpress-cloning-script.py
@@ -1,11 +1,24 @@
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
-def main(environment, post_id):
-    base_url = "https://info.qa.orcid.org/" if environment == "QA" else "https://info.orcid.org/"
-    headers = {'Accept': 'application/json',     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 
-    response = requests.get(f"{base_url}wp-json/wp/v2/posts/{post_id}", headers=headers)
+def main(environment, post_id, wordpreess_staggin_username, wordpreess_staggin_password):
+    # Define the base URL based on the environment
+    base_url = "https://info.qa.orcid.org/" if environment != "PROD" else "https://info.orcid.org/"
+    headers = {
+        'Accept': 'application/json',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+
+    # Setup authentication if not in production environment
+    auth = None
+    if environment != "PROD":
+        auth = HTTPBasicAuth(wordpreess_staggin_username, wordpreess_staggin_password)
+
+    # Make the request with or without authentication based on the environment
+    response = requests.get(f"{base_url}wp-json/wp/v2/posts/{post_id}", headers=headers, auth=auth)
+
     print(f"Fetching post {post_id} from {base_url}wp-json/wp/v2/posts/{post_id}")
     if response.status_code == 200:
         html_content = response.json()["content"]["rendered"]
@@ -16,10 +29,12 @@ def main(environment, post_id):
         print(f"Failed to fetch post: {response}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python wordpress-cloning-script.py <environment> <postID>")
+    if len(sys.argv) != 5:
+        print("Usage: python wordpress-cloning-script.py <environment> <postID> <wordpreess_staggin_username> <wordpreess_staggin_password>")
         sys.exit(1)
     
     env = sys.argv[1]
     post_id = sys.argv[2]
-    main(env, post_id)
+    wordpreess_staggin_username = sys.argv[3]
+    wordpreess_staggin_password = sys.argv[4]
+    main(env, post_id, wordpreess_staggin_username, wordpreess_staggin_password)
