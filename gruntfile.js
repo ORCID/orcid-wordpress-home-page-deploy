e63d516ff55e6207dc8dc9af90a014d8a7801598
Leonardo Mendoza Fernadez <l.mendoza@Leonardos-MacBook-Pro.local>
2024-05-08 08:21:39 -0600

    Clone wp post

--- gruntfile.js
+++ gruntfile.js
@@ -8,7 +8,21 @@ module.exports = function(grunt) {
                 options: {
                 },
                 files: {
-                    'dist/index.html': 'dist/index.html'
+                    'dist/index.html': 'dist/index.html',
+                //     "dis/index-ar.html": "dist/index-ar.html",
+                //     "dist/index-cs.html": "dist/index-cs.html",
+                //     "dist/index-de.html": "dist/index-de.html",
+                //     "dist/index-es.html": "dist/index-es.html",
+                //     "dist/index-fr.html": "dist/index-fr.html",
+                //     "dist/index-it.html": "dist/index-it.html",
+                //     "dist/index-ja.html": "dist/index-ja.html",
+                //     "dis/index-ko.html": "dist/index-ko.html",
+                //     "dist/index-pl.html": "dist/index-pl.html",
+                //     "dist/index-pt.html": "dist/index-pt.html",
+                //     "dist/index-ru.html": "dist/index-ru.html",
+                //     "dist/index-tr.html": "dist/index-tr.html",
+                //     "dist/index-zh-CN.html": "dist/index-zh-CN.html",
+                //     "dist/index-zh-TW.html": "dist/index-zh-TW.html",
                 }
             }
         }
--- wordpress-cloning-html-script.py
+++ wordpress-cloning-html-script.py
@@ -3,9 +3,30 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-def main(environment, post_id, wordpreess_staggin_username, wordpreess_staggin_password):
+# Langauge dictionary
+language_dict = {
+    "en" : "",
+    "ar" : "ar",
+    "cs" : "cs",
+    "de" : "de",
+    "es" : "es",
+    "fr" : "fr",
+    "it" : "it",
+    "ja" : "ja",
+    "ko" : "ko",
+    "pl" : "pl",
+    "pt" : "pt",
+    "ru" : "ru",
+    "tr" : "tr",
+    "zh-CN" : "zh-CN",
+    "zh-TW" : "zh-TW"
+}
+
+def main(environment, post_id, wordpreess_staggin_username, wordpreess_staggin_password, language):
     # Define the base URL based on the environment
     base_url = "https://info.qa.orcid.org/" if environment != "PROD" else "https://info.orcid.org/"
+    if language:
+        base_url = f"{base_url}{language}/"
     headers = {
         'Accept': 'application/json',
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
@@ -19,7 +40,9 @@ def main(environment, post_id, wordpreess_staggin_username, wordpreess_staggin_p
     # Make the request with or without authentication based on the environment
     response = requests.get(f"{base_url}wp-json/wp/v2/pages/{post_id}", headers=headers, auth=auth)
 
-    with open(f"dist/index.html", "w") as file:
+    file_path = f"dist/index{'' if language == '' else '-' + language}.html"
+
+    with open(file_path, "w") as file:
         file.write("<doctype html><html><head><link rel=\"stylesheet\" href=\"./combined_styles.css\"></head><body>")
 
         if response.status_code == 200:
@@ -35,12 +58,18 @@ def main(environment, post_id, wordpreess_staggin_username, wordpreess_staggin_p
 
 if __name__ == "__main__":
     print(sys.argv)
-    if len(sys.argv) != 5:
-        print("Usage: python wordpress-cloning-script.py <environment> <postID> <wordpreess_staggin_username> <wordpreess_staggin_password>")
+    if len(sys.argv) != 6:
+        print("Usage: python wordpress-cloning-script.py <environment> <postID> <wordpreess_staggin_username> <wordpreess_staggin_password> <include_languages>")
         sys.exit(1)
     
     env = sys.argv[1]
     post_id = sys.argv[2]
     wordpreess_staggin_username = sys.argv[3]
     wordpreess_staggin_password = sys.argv[4]
-    main(env, post_id, wordpreess_staggin_username, wordpreess_staggin_password)
+    include_languages = sys.argv[5]
+
+    if (include_languages == "true"):
+        for key, value in language_dict.items():
+            main(env, post_id, wordpreess_staggin_username, wordpreess_staggin_password, value)
+    else:
+        main(env, post_id, wordpreess_staggin_username, wordpreess_staggin_password, include_languages)
