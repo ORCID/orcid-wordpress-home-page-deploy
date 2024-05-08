0853e48eb48c848f6dcafd0d2efb8d183ac6ab69
Leonardo Mendoza Fernadez <l.mendoza@Leonardos-MacBook-Pro.local>
2024-05-07 19:09:21 -0600

    Clone wp post

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        // Configuration for inline-css
+        inlinecss: {
+            main: {
+                options: {
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html'
+                }
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-inline-css');
+
+    // Default task(s).
+    grunt.registerTask('default', ['inlinecss']);
+};
