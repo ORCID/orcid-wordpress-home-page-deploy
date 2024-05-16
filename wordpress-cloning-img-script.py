48fae2cc118c49b90ad9832f1fff0f7c998ceef3
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2024-05-15 18:00:49 -0600

    Clone wp post

--- wordpress-cloning-img-script.py
+++ wordpress-cloning-img-script.py
@@ -6,7 +6,7 @@ import requests
 import shutil
 from requests.auth import HTTPBasicAuth
 
-def download_and_update_html( environment,wordpreess_staggin_username, wordpreess_staggin_password,folder_path='./dist/'):
+def download_and_update_html(environment, wordpress_staging_username, wordpress_staging_password, folder_path='./dist/'):
 
     headers = {
         'Accept': 'application/json',
@@ -16,7 +16,7 @@ def download_and_update_html( environment,wordpreess_staggin_username, wordprees
     # Setup authentication if not in production environment
     auth = None
     if environment != "PROD":
-        auth = HTTPBasicAuth(wordpreess_staggin_username, wordpreess_staggin_password)
+        auth = HTTPBasicAuth(wordpress_staging_username, wordpress_staging_password)
 
     # Create directory for saving images if it does not exist
     if not os.path.exists(folder_path):
@@ -29,7 +29,7 @@ def download_and_update_html( environment,wordpreess_staggin_username, wordprees
     # Find all image tags
     images = soup.find_all('img')
 
-    # Loop through all images, download them, and update their src attributes
+    # Loop through all images, download them, and update their src and srcset attributes
     for img in images:
         # Get the source attribute of the image
         img_url = img.get('src')
@@ -46,19 +46,41 @@ def download_and_update_html( environment,wordpreess_staggin_username, wordprees
                 img_data.raw.decode_content = True
                 shutil.copyfileobj(img_data.raw, file)
             # Update the src attribute to the new local path
-            img['src'] = os.path.join('./',local_filename)
+            img['src'] = os.path.join('./', local_filename)
+        
+        # Get the srcset attribute of the image
+        img_srcset = img.get('srcset')
+        if img_srcset:
+            new_srcset = []
+            srcset_items = img_srcset.split(',')
+            for item in srcset_items:
+                url, size = item.strip().split(' ')
+                full_img_url = url
+                # Download the image
+                img_data = requests.get(full_img_url, stream=True, headers=headers, auth=auth)
+                # Define the new local filename
+                local_filename = os.path.basename(url)
+                # Save the image locally
+                local_filepath = os.path.join(folder_path, local_filename)
+                with open(local_filepath, 'wb') as file:
+                    img_data.raw.decode_content = True
+                    shutil.copyfileobj(img_data.raw, file)
+                # Add the new srcset item to the list
+                new_srcset.append(f"{os.path.join('./', local_filename)} {size}")
+            # Update the srcset attribute to the new local paths
+            img['srcset'] = ', '.join(new_srcset)
 
     # Write the updated HTML to a new file
-    with open( f"dist/index.html" , 'w') as file:
+    with open(f"dist/index.html", 'w') as file:
         file.write(str(soup))
 
 if __name__ == "__main__":
     print(sys.argv)
     if len(sys.argv) != 4:
-        print("Usage: python wordpress-cloning-script.py <environment> <wordpreess_staggin_username> <wordpreess_staggin_password>")
+        print("Usage: python wordpress-cloning-script.py <environment> <wordpress_staging_username> <wordpress_staging_password>")
         sys.exit(1)
     
     env = sys.argv[1]
-    wordpreess_staggin_username = sys.argv[2]
-    wordpreess_staggin_password = sys.argv[3]
-    download_and_update_html(env, wordpreess_staggin_username, wordpreess_staggin_password)
+    wordpress_staging_username = sys.argv[2]
+    wordpress_staging_password = sys.argv[3]
+    download_and_update_html(env, wordpress_staging_username, wordpress_staging_password)
