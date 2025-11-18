e5009b48b764bcfa650700edfe53be99633568c6
Rob Blackburn <r.blackburn@orcid.org>
2025-11-18 14:18:22 +0000

    Update wordpress-cloning-css-script.py more css for new block

--- wordpress-cloning-css-script.py
+++ wordpress-cloning-css-script.py
@@ -23,6 +23,7 @@ def clone_css_files(environment, wordpress_staging_username, wordpress_staging_p
         f"{base_url}wp-content/themes/orcid-outreach-pro/homepage.css",
         f"{base_url}/wp-content/uploads/siteorigin-widgets/sow-tabs-default-aecd6fef7ad0.css",
         f"{base_url}/wp-content/plugins/advanced-tabs-block/build/global/global.css",
+        f"{base_url}/wp-content/plugins/advanced-tabs-block/build/blocks/tabs/style-index.css",
 
  
     ]
